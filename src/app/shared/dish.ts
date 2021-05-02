15c492fe151b8e6ee797b4c38867ca354e2324fe
Kartik Kshirsagar <kartikpkshirsagar@gmail.com>
2021-05-02 13:41:57 +0530

    component generated

---
+++ src/app/shared/dish.ts
@@ -0,0 +1,11 @@
+export class Dish{
+    id!: string; // ! is a ts literal which tells bascally that this variable will have a value at runtime
+    name!: string;
+    image!: string;
+    category!: string;
+    featured!: boolean;
+    label!: string;
+    description!: string;
+    price!:string;
+
+}
