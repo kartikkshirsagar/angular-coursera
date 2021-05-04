ee45aa7b3f9a2933d4e1cb16d405ec228213e466
Kartik Kshirsagar <kartikpkshirsagar@gmail.com>
2021-05-04 14:40:15 +0530

    started routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,30 @@ import {MatGridListModule} from '@angular/material/grid-list';
 import {MatListModule} from '@angular/material/list';
 import{MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
+
 import 'hammerjs';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishDetailComponent } from './dish-detail/dish-detail.component';
-import {DishService} from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
+import {DishService} from './services/dish.service';
+import {AppRoutingModule} from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishDetailComponent
+    DishDetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
@@ -29,6 +41,7 @@ import {DishService} from './services/dish.service';
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
+    AppRoutingModule,
   ],
   providers: [
     DishService,
