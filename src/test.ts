f248bbc67fbab8f69c4460582bc07400e41c835d
Kartik Kshirsagar <kartikpkshirsagar@gmail.com>
2021-05-09 23:09:08 +0530

    testing

--- src/test.ts
+++ src/test.ts
@@ -20,6 +20,6 @@ getTestBed().initTestEnvironment(
   platformBrowserDynamicTesting()
 );
 // Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
+const context = require.context('./', true, /menu.component\.spec\.ts$/); //edited to just test menu component
 // And load the modules.
 context.keys().map(context);
